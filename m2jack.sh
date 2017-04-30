48d6fcf4f38fe2440a42183c7e49a5975873f320
Chris <cparks13@live.com>
2017-04-30 17:35:18 -0400

    Update m2jack.sh
    Added some brief instructions on how to get this script to be run on boot of the Raspberry Pi

--- m2jack.sh
+++ m2jack.sh
@@ -3,6 +3,10 @@
 # directly to the Raspberry PI's audio (3.5mm) jack.
 # Engineer: Christopher Parks (cparks 13 AT live DOT com)
 
+# Create directory "MIDI2jack" in "/usr/bin/".
+# At the following line to the end of "~/.profile" (~ stands for your home dir):
+# "/usr/bin/MIDI2jack/m2jack.sh"
+
 fluidsynth -is --audio-driver=alsa --gain 6 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
 sleep 10
 aconnect 20 128
